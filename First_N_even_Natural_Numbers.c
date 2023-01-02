a408aa81f35c72844e65249e264d646af5e762c7
vaishu1205 <119730123+vaishu1205@users.noreply.github.com>
2023-01-02 18:28:33 +0530

    #include <stdio.h> int main() {     int n,i;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         //if(i%2==0) //          printf("%d ",i*2);         //}     }
    }

---
+++ First_N_even_Natural_Numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n,i; 
+    scanf("%d",&n);
+    for(i=n;i>=1;i--) 
+    {
+        //if(i%2==0) //
+         printf("%d ",i*2);
+        //}
+    }
+        
+}
